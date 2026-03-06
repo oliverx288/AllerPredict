a7b028669c3e79178d61740e26977357a06b4181
Irene Oliver <irene.oliver.oliver.17@gmail.com>
2026-03-06 22:35:04 +0100

    feat: clases modelo Sintoma, Alergeno y Paciente

---
+++ src/model/Sintoma.java
@@ -0,0 +1,39 @@
+package model;
+public class Sintoma {
+
+    private String nombre;
+
+    public Sintoma(String nombre) {
+        this.nombre = nombre.toLowerCase().trim();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Sintoma other = (Sintoma) obj;
+        if (nombre == null) {
+            if (other.nombre != null)
+                return false;
+        } else if (!nombre.equals(other.nombre))
+            return false;
+        return true;
+    }
+ 
+}
